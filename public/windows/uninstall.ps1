01bf32db50b3f403d24fab96f6353ff98ddbb8db
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2023-02-27 02:53:26 +0900

    feat: add install.ps1 and uninstall.ps1.

---
+++ public/windows/uninstall.ps1
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2023 Susumu OTA <1632335+susumuota@users.noreply.github.com>
+# SPDX-License-Identifier: MIT
+
+$path = "HKCU:\Software\Google\Chrome\NativeMessagingHosts\io.github.susumuota.nostr_keyx"
+
+Remove-Item -Path $path -Force
+
+"Done!"
--- public/windows/uninstall_host.bat
+++
@@ -1,6 +0,0 @@
-:: SPDX-FileCopyrightText: 2023 Susumu OTA <1632335+susumuota@users.noreply.github.com>
-:: SPDX-License-Identifier: MIT
-::
-:: https://developer.chrome.com/docs/apps/nativeMessaging/#native-messaging-host-location
-
-REG DELETE "HKLM\Software\Google\Chrome\NativeMessagingHosts\io.github.susumuota.nostr_keyx" /f
