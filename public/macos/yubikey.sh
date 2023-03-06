405c597b6559ae5bcd26b26785dc2b539302a142
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2023-03-06 23:33:00 +0900

    fix: typo.

--- public/macos/yubikey.sh
+++ public/macos/yubikey.sh
@@ -41,7 +41,7 @@ privatekey="nostr_privatekey.asc"
 script_dir=$(cd $(dirname $0); pwd)
 privatekey_path="${script_dir}/$(basename $privatekey)"
 
-# decript the private key with gpg and yubikey
+# decrypt the private key with gpg and yubikey
 # see details about options
 # https://www.gnupg.org/documentation/manuals/gnupg/GPG-Configuration-Options.html
 $gpg_path --batch --no-tty --status-fd 2 --with-colons -q --yes -d "$privatekey_path"
