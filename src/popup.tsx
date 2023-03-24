02ca61dfe904a8b997ab48193b87a7ca2c9a0928
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2023-03-25 03:21:36 +0900

    feat: add prompt page.

--- src/popup.tsx
+++ src/popup.tsx
@@ -6,5 +6,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
 
 const container = document.getElementById('app');
-const root = createRoot(container!);
-root.render(<App />);
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
